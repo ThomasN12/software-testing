f582a83be7501184a8077745a879c41b64a9a15f
ThomasN12 <phuocthinhdn12023@gmail.com>
2023-10-25 20:24:30 -0400

    finish mutation

--- test_suit/test_mutationAdequate.py
+++ test_suit/test_mutationAdequate.py
@@ -62,13 +62,33 @@ class TestMutationAdequate(unittest.TestCase):
         expected = Triangle.Type.INVALID
         self.assertEqual(actual, expected)
 
-    # def test13(self):
-    #     actual = Triangle.classify(11, 12, 13)
-    #     expected = Triangle.Type.SCALENE
-    #     self.assertEqual(actual, expected)
-    # def test10(self):
-    #   actual = Triangle.classify(1, 2, 3)
-    #   expected = Triangle.Type.INVALID
-    #   self.assertEqual(actual, expected)
+    def test13(self):
+        invalid = Triangle.classify(-3, -2, -4)
+        scalene = Triangle.classify(3, 4, 5)
+        equilateral = Triangle.classify(3, 3, 3)
+        isosceles = Triangle.classify(3, 3, 4)
+        self.assertEqual(invalid.value, 0)
+        self.assertEqual(scalene.value, 1)
+        self.assertEqual(equilateral.value, 2)
+        self.assertEqual(isosceles.value, 3)
+
+    def test14(self):
+        actual = Triangle.classify(1, 1, 1)
+        expected = Triangle.Type.EQUILATERAL
+        self.assertEqual(actual, expected)
+
+    def test15(self):
+        actual1 = Triangle.classify(2, 2, 4)
+        actual2 = Triangle.classify(4, 2, 2)
+        actual3 = Triangle.classify(2, 4, 2)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual1, expected)
+        self.assertEqual(actual2, expected)
+        self.assertEqual(actual3, expected)
+
+    def test16(self):
+        actual = Triangle.classify(5, 5, 0)
+        expected = Triangle.Type.INVALID
+        self.assertEqual(actual, expected)
 if __name__ == '__main__':
   unittest.main()
